6ce8f80b8706a3e0e3dab9b2eff9ff7294e9fbaf
Wilver Arana R <u202023992@upc.edu.pe>
2023-11-27 18:57:16 -0500

    feat(Main): Endpoint updated

--- src/main/java/com/upc/demo_docker/DemoDockerApplication.java
+++ src/main/java/com/upc/demo_docker/DemoDockerApplication.java
@@ -2,6 +2,7 @@ package com.upc.demo_docker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +18,10 @@ public class DemoDockerApplication {
 	@GetMapping("/greet/{name}")
 	public String greeting(@PathVariable String name) {
 		return "Hello, " + name;
+	}
+
+	@GetMapping("/status")
+	public ResponseEntity<String> checkStatus() {
+		return ResponseEntity.ok("Hello, WilverAR");
 	}
 }
