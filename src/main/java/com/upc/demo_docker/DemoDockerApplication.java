cad9251e3bdb55dda12611ce9bc26018f106e253
Wilver Arana R <u202023992@upc.edu.pe>
2023-11-27 19:19:34 -0500

    feat(Main): Endpoint - updated

--- src/main/java/com/upc/demo_docker/DemoDockerApplication.java
+++ src/main/java/com/upc/demo_docker/DemoDockerApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("api/demo")
 public class DemoDockerApplication {
 
 	public static void main(String[] args) {
